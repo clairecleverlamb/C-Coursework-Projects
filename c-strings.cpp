dd27a30b5cfda30543cd13186900f8cfa9bb2c59
clairecleverlamb <clairechenszeying@gmail.com>
2025-03-02 23:49:08 -0800

    adding c-string files

--- c-strings.cpp
+++ c-strings.cpp
@@ -43,38 +43,35 @@ void reverse(char *s){
 // For example, if s is “go giants”, target is ‘g’, and replacement is ‘G’, the function should change s to “Go Giants” and return 2.
 
 int replace(char *s, char target, char replacementChar){
-    
 }
  
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-4.  This function returns the index in string s where the substring can first be found. For example if s is “Skyscraper” and substring is “ysc” the function would return 2.  It should return -1 if the substring does not appear in the string.
+// 4.  This function returns the index in string s where the substring 
+// can first be found. For example if s is “Skyscraper” and substring is “ysc” the function would return 2.  
+// It should return -1 if the substring does not appear in the string.
 
 int findSubstring(char *s, char substring[])
  
 
-5. This function returns true if the argument string is a palindrome. It returns false if it is not.  A palindrome is a string that is spelled the same as its reverse.  For example “abba” is a palindrome. So is “hannah”, “abc cba”, and “radar”.
+// 5. This function returns true if the argument string is a palindrome. 
+// It returns false if it is not.  A palindrome is a string that is spelled the same as its reverse.  
+// For example “abba” is a palindrome. So is “hannah”, “abc cba”, and “radar”.
 
 bool isPalindrome(char *s)
 Note: do not get confused by white space characters. They should not get any special treatment.  “abc ba” is not a palindrome. It is not identical to its reverse.
 
  
 
-6. Extra Credit, up to 10 extra points.  This function should reverse the words in a string.  A word can be considered to be any characters, including punctuation, separated by spaces (only spaces, not tabs, \n etc.).  So, for example, if s is “The Giants won the Pennant!” the function should change s to “Pennant! the won Giants The”
+// 6. Extra Credit, up to 10 extra points.  This function should reverse the words in a string.
+//   A word can be considered to be any characters, including punctuation, 
+//   separated by spaces (only spaces, not tabs, \n etc.).  
+//   So, for example, if s is “The Giants won the Pennant!” the function should change s to “Pennant! the won Giants The”
 
 void reverseWords(char *s)
+
+
 
 
 
